f6e395440ce031a1808abe32816743f0e419c856
Boris Grunwald <borisgrunwald@gmail.com>
2022-03-22 20:13:07 +0100

    removed getRandomIntInclusive

--- index.js
+++ index.js
@@ -187,11 +187,6 @@ range.addEventListener('input', e => {
 
 
 
-function getRandomIntInclusive(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
-}
 
 function getRandomBoolean() {
     return Math.random() >= 0.5;
